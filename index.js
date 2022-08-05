2026e566298ffb0fc16795cf455351bd552ce188
rayct <rayct@icloud.com>
2022-08-05 02:04:53 +0100

    Fetched the Date String with params.date_string

--- index.js
+++ index.js
@@ -21,7 +21,14 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
+  console.log({ greeting: 'hello API' });
   res.json({greeting: 'hello API'});
+});
+
+app.get("/api/timestamp/:date_string", function(req, res) {
+  let dateString = req.params.date_string;
+  console.log(dateString);
+  res.json({"error" : "Invalid Date"});
 });
 
 

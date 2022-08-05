e26bed2d3284887483fa6f81c3688689577e4fe7
rayct <rayct@icloud.com>
2022-08-05 16:43:55 +0100

    Syntax Space added

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.get("/api/hello", function (req, res) {
 app.get("/api/timestamp/:date_string", function(req, res) {
   let dateString = req.params.date_string;
   console.log(dateString);
+  
   res.json({"error" : "Invalid Date"});
 });
 
